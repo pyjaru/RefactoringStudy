ebb2cf3b6810e2bb6a7a7310a47ba80bde69702c
pyjaru <pyjaru@gmail.com>
2018-11-01 11:54:55 +0900

    1장. 책의 코드와 유사하게 Statement 메서드에 foreach 대신 while문을 사용

--- VideoStore/Customer.cs
+++ VideoStore/Customer.cs
@@ -30,37 +30,41 @@ namespace VideoStore
         {
             double totalAmount = 0;
             int frequentRenterPoint = 0;
+            var rentals = _rentals.GetEnumerator();
             string result = $"Rental Record for {this.Name}\n";
-            foreach(var rental in _rentals)
+
+            while (rentals.MoveNext())
             {
                 double thisAmount = 0;
-                switch (rental.Movie.PriceCode)
+                Rental each = rentals.Current;
+
+                switch (each.Movie.PriceCode)
                 {
                     case Movie.REGULAR:
                         thisAmount += 2;
-                        if (rental.DaysRented > 2)
-                            thisAmount += (rental.DaysRented - 2) * 1.5;
+                        if (each.DaysRented > 2)
+                            thisAmount += (each.DaysRented - 2) * 1.5;
                         break;
                     case Movie.NEW_RELEASE:
-                        thisAmount += rental.DaysRented * 3;
+                        thisAmount += each.DaysRented * 3;
                         break;
                     case Movie.CHILDREN:
                         thisAmount += 1.5;
-                        if (rental.DaysRented > 3)
-                            thisAmount += (rental.DaysRented - 3) * 1.5;
+                        if (each.DaysRented > 3)
+                            thisAmount += (each.DaysRented - 3) * 1.5;
                         break;
                 }
-
                 //포인트(frequent, renter oints)추가
                 frequentRenterPoint++;
                 //최신(new release)을 이틀 이상 대여하는 경우 추가 포이늩 제공
-                if ((rental.Movie.PriceCode == Movie.NEW_RELEASE) && (rental.DaysRented > 1))
+                if ((each.Movie.PriceCode == Movie.NEW_RELEASE) && (each.DaysRented > 1))
                     frequentRenterPoint++;
 
                 //이 대여에 대한 요금 계산 결과 표시
-                result += $"\t{rental.Movie.Title}\t{thisAmount}\n";
+                result += $"\t{each.Movie.Title}\t{thisAmount}\n";
                 totalAmount += thisAmount;
             }
+
             // 풋터(footer)추가
             result += $"Amount owed is {totalAmount}\n";
             result += $"You earned {frequentRenterPoint} frequent reter points";
